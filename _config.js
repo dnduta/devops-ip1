f2d731877bcfc607c284fd74c5fd8a5ac02c5963
Gitau nduta <gitaudennis12@gmail.com>
2020-08-18 12:33:26 +0300

    Add atlas mongo db configuration

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://mor-db-user:TvT0AeESfNG5VVZl@ip3-moringa.kq5e1.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    development: 'mongodb+srv://mor-db-user:TvT0AeESfNG5VVZl@ip3-moringa.kq5e1.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    test: 'mongodb+srv://mor-db-user:TvT0AeESfNG5VVZl@ip3-moringa.kq5e1.mongodb.net/<dbname>?retryWrites=true&w=majority',
 }
 module.exports = config;
